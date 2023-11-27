0060579339ac0cbd60f2b46ae4d34a181207de9c
m-nanda <m.nanda98@hotmail.com>
2023-11-27 16:32:48 +0700

    update folder & images

--- Module 2 - End-to-End ML System/src/generate_report.py
+++ Module 2 - End-to-End ML System/src/generate_report.py
@@ -1,6 +1,5 @@
 import pandas as pd
-import json
-import requests
+import json, requests, os
 import seaborn as sns
 import matplotlib.pyplot as plt
 from config import *
@@ -75,7 +74,7 @@ def make_report(trained_data: pd.DataFrame, new_data: pd.DataFrame) -> None:
     
     fig, axes = plt.subplots(nrows=4, ncols=1, figsize=(12, 12))
     for i, col in enumerate(new_data.columns[:-1]):
-        # Plot histogram unutk perbandingan data latih dan data baru
+        # Plot histogram for both trained and new data
         sns.histplot(trained_data[col], bins=20, palette="crest", label='Trained Data', ax=axes[i], stat="percent")
         sns.histplot(new_data[col], bins=20, palette="rocket_r", label='New Data', ax=axes[i], stat="percent")
         axes[i].set_title(col)
@@ -87,7 +86,20 @@ def make_report(trained_data: pd.DataFrame, new_data: pd.DataFrame) -> None:
     plt.savefig('../report/komparasi_data.pdf', dpi=300)
     new_data.to_csv("../report/data_baru.csv", index=False)
 
+def prepare_directory() -> None:
+    """
+    Menyiapkan direktori tempat data akan disimpan.
+
+    Returns:
+        str: Path ke direktori data
+    """
+    base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+    report_dir = f"{base_dir}/report"
+    if not os.path.exists(report_dir):
+        os.makedirs(report_dir)
+
 def create_report():
+    prepare_directory()
     new_data = simulation(get_samples())
     trained_data = load_trained_data()
     make_report(trained_data, new_data)
